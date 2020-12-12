7f0da3ec9e3e015f8535acab3552ee6a03c7fc6a
Sanidhya Pawar <37259966+pawarss1@users.noreply.github.com>
2020-12-12 18:42:35 +0530

    .

--- src/index.js
+++ src/index.js
@@ -24,8 +24,8 @@ app.get("/findColleges", async (req, res) => {
     if(req.query.course){
         queryCond.course={$regex:req.query.course,$options:"i"};
     }
-    if(req.query.exams){
-        queryCond.exam =  {$regex:req.query.exams, $options:"i"};
+    if(req.query.exam){
+        queryCond.exam =  {$regex:req.query.exam, $options:"i"};
     }
     if(req.query.minPackage && !isNaN(Number(req.query.minPackage)) && Number(req.query.minPackage) > 0){
         const curPack = Number(req.query.minPackage);
